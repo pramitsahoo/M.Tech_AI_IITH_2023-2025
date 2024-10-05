29c4c6b50893b9892371b0136f93027961303252
Pramit Sahoo <pramitsahoo.gnipst@gmail.com>
2024-10-05 16:08:16 +0530

    Add files via upload

---
+++ Semester 2/AI5100 Deep Learning/Code_Submission/QA_Models/predict.sh
@@ -0,0 +1,4 @@
+python predict_model.py \
+--input test.json \
+--output output.jsonl \
+--model "pramitsahoo/deberta-paqott"
---
+++ Semester 2/AI5100 Deep Learning/Code_Submission/QA_Models/run_training.sh
@@ -0,0 +1,10 @@
+python hf_qa_train.py \
+--train-data train.json \
+--val-data validation.json \
+--model-name MateuszW/deberta-paqott \
+--output-dir models/deberta-finetuned \
+--batch-size 2 \
+--epochs 10 \
+--learning-rate 2e-6 \
+--max-length 512
+
